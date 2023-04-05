935cf934cddb0bbacba2524019a69b15e34139f0
Prince kumar Singh <122429010+singhprince9875@users.noreply.github.com>
2023-04-05 22:01:06 +0530

    Add files via upload

---
+++ sumoftherow.c
@@ -0,0 +1,21 @@
+/*#include<stdio.h>
+int main(){int sum = 0;
+
+    int arr[2][2]={{1,2},{3,4}};
+    for(int i=0;i<2;i++)
+        for(int j=0;j<2;j++)
+    
+    for(int i=0;i<2;i++){
+        sum = sum+arr[j];}
+        for(int j=0;j<2;j++)
+      printf("%d",arr[i][j]); 
+    
+    
+}*/
+// given a matrix having 0-1,onlyfind the row with maximum number of 1s.
+#include<stdio.h>
+int main(){
+    int arr
+}
+
+
